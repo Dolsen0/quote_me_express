f7fd02b9dbcbd589cea7b6170fa7d8631ac0ce70
dolsen0 <derek.olsen@gmail.com>
2023-08-22 17:43:51 -0400

    adjustment to getauthors controller

--- controllers/GetAuthors.js
+++ controllers/GetAuthors.js
@@ -1,31 +1,30 @@
 import axios from "axios";
 
 export const getAuthors = async (req, res, next) => {
-        try {
-        let authors = new Set();
-        let skip = 0;
-        let hasMore = true;
+  try {
+    let authors = new Set();
+    let skip = 0;
+    let hasMore = true;
 
-        while (hasMore) {
-            const response = await axios.get('https://api.quotable.io/quotes', {
-                params: {
-                    limit: 100,
-                    skip: skip
-                }
-            });
+    while (hasMore) {
+      const response = await axios.get("https://api.quotable.io/quotes", {
+        params: {
+          limit: 100,
+          skip: skip,
+        },
+      });
 
-            const quotes = response.data.results;
-            for (let quote of quotes) {
-                authors.add(quote.author);
-            }
+      const quotes = response.data.results;
+      for (let quote of quotes) {
+        authors.add(quote.author);
+      }
 
-            // Check if there are more quotes to fetch
-            hasMore = quotes.length === 100;
-            skip += 100;
-        }
-
-        res.json(Array.from(authors));
-    } catch (error) {
-        res.status(500).send('Error fetching authors');
+      hasMore = quotes.length === 100;
+      skip += 100;
     }
-}
+
+    res.json(Array.from(authors));
+  } catch (error) {
+    res.status(500).send("Error fetching authors");
+  }
+};
